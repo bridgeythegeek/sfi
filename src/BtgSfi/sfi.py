feca188fc0a2f624a01965ff38e3e23bf18b494a
bridgeythegeek <bridgeythegeek@gmail.com>
2021-08-11 14:29:17 +0100

    Alpha of rules

--- src/BtgSfi/sfi.py
+++ src/BtgSfi/sfi.py
@@ -1,8 +1,8 @@
 import argparse
 import concurrent.futures
 import inspect
+import json
 import logging
-import openpyxl
 import os
 import tqdm
 
@@ -17,17 +17,25 @@ class sfi:
         'windows.old': 'windows'
     }
 
-    _win_dirs = []    
-
-    def __init__(self, items, max_workers=3, items_per_thread=1000, winexe=None):
+    def __init__(self, items, max_workers=3, items_per_thread=1000, winexe_file=None, rules_files=None):
+        
         self.items = items
         self.max_workers = max_workers
         self.items_per_thread = items_per_thread
         self.have_errors = False
-        if winexe is None:
-            winexe = os.path.join(os.path.dirname(os.path.abspath(inspect.stack()[0].filename)), 'winexe.txt')
+
+        # Read rules file
+        self.rules = []
+        if not rules_files is None:
+            for rules_file in rules_files:
+                self.rules.extend(self.validate_rules(rules_file))
+        logging.info(f"Got {len(self.rules)} rules.")
+        
+        # Read winexe file
+        if winexe_file is None:
+            winexe_file = os.path.join(os.path.dirname(os.path.abspath(inspect.stack()[0].filename)), 'winexe.txt')
         self.winexes = {}
-        with open(winexe) as f:
+        with open(winexe_file) as f:
             winexes_ = [x.strip().lower() for x in f.readlines() if not x.startswith('#')]
         winexes_ = set(winexes_)
         logging.debug(f"Read {len(winexes_):,} known goods.")
@@ -71,9 +79,104 @@ class sfi:
             if not resolve:
                 return item.rsplit('/', 1)
         
-        # Err??
+        # Assume it's just a file name, no path
         else:
-            return [None, None]
+            return ['', item]
+
+    def validate_rules(self, rules_file):
+        logging.info(f"Validating rules file {rules_file!r}.")
+        
+        j = None
+        with open(rules_file) as f:
+            rules = json.load(f)
+        
+        if not rules:
+            return None
+        
+        new_rules = []
+        all_valid = True
+        for rule_i, rule in enumerate(rules):
+            if 'enabled' in rule and rule['enabled'] == False:
+                logging.info(f"Rule {rule_i} is disabled.")
+                continue
+
+            valid = True
+            
+            if not all([x in rule.keys() for x in ('name', 'conditions')]):
+                logging.error(f"Rule {rule_i}, must contain both 'name' and 'conditions'.")
+                valid = all_valid = False
+                
+            for key in rule.keys():
+                if not key in ('name', 'enabled', 'comment', 'conditions'):
+                    logging.error(f"Invalid key in rule {rule_i}: {key}")
+                    valid = False
+                    continue
+                if key in ('name', 'comment') and not isinstance(rule[key], str):
+                    logging.error(f"In rule {rule_i}, key {key} must be a string.")
+                    valid = False
+                    continue
+                if key in ('enabled'):
+                    logging.debug(type(rule[key]))
+                if key in ('conditions'):
+                    if isinstance(rule[key], list):
+                        for condition in rule[key]:
+                            pass
+                    else:
+                        logging.error(f"In rule {rule_i}, key {key} must be a list.")
+                        valid = False
+                        continue
+
+            if valid:
+                new_rules.append(rule)
+            else:
+                all_valid = False
+        
+        if not all_valid:
+            return False
+        
+        return new_rules
+    
+    @staticmethod
+    def check_rule(rule, item, path_, base):
+        logging.debug(f"{rule['name']=}, {item=}, {path_=}, {base=}")
+
+        matched = False
+        for condition in rule['conditions']:
+
+            case = False
+            values = condition['value']
+            if isinstance(values, str):
+                values = [values]
+
+            if 'case' in condition and condition['case']:
+                case = True
+            if not case:
+                item = item.lower()
+                path_ = path_.lower()
+                base = base.lower()
+                values = [v.lower() for v in values]
+            
+            element = item
+            if condition['element'] == 'path':
+                element = path_
+            elif condition['element'] == 'base':
+                element = base
+            del(item)
+            del(path_)
+            del(base)
+            
+            if condition['criteria'] == 'contains':
+                matched = any([v in element for v in values])
+            elif condition['criteria'] == 'is':
+                matched = any([v == element for v in values])
+            elif condition['criteria'] == 'starts':
+                matched = any([element.startswith(v) for v in values])
+            elif condition['criteria'] == 'ends':
+                matched = any([element.endswith(v) for v in values])
+            else:
+                logging.error(f"In rule {rule['name']!r}, unknown criteria: {condition['criteria']}")
+
+        return matched
 
     def execute(self, i):
         result = []
@@ -82,18 +185,21 @@ class sfi:
                 matches = []
 
                 # Check WinExe first.
-                path, base = sfi.split_path(item)
-                if path in self.winexes:
-                    if not base in self.winexes[path]:
+                path_, base = sfi.split_path(item)
+                if path_ in self.winexes:
+                    if not base in self.winexes[path_]:
                         matches.append('NoWin')
+                
+                # Then the rules
+                for rule in self.rules:
+                    if sfi.check_rule(rule, item, path_, base):
+                        matches.append(rule['name'])
+                
                 if len(matches) > 0:
                     result.append((item, matches))
 
-                # Then the rules
-                # TODO
-
             except Exception as ex:
-                logging.debug(f"Error whilst processing {item!r}: {ex}")
+                logging.error(f"Error whilst processing {item!r}: {ex.with_traceback()}")
                 self.have_errors = True
             
             self.pbar.update(1)
@@ -117,14 +223,17 @@ class sfi:
 if __name__ == '__main__':
 
     argp = argparse.ArgumentParser()
-    argp.add_argument('file')
+    argp.add_argument('--file', '-f', metavar='files.txt', help="Text file of file paths to check")
+    argp.add_argument('--winexe', metavar="winexe.txt", help="Text file of known good Windows exes")
+    argp.add_argument('--rules', metavar="rules.json", help="Rules to detect evil", nargs="+")
+    argp.add_argument('--debug', action="store_true", help="Debug level")
     args = argp.parse_args()
 
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.INFO)
 
     with open(args.file, encoding='utf-8') as f:
         todo = [x.strip().lower() for x in f.readlines() if not x.startswith('#')]
     logging.debug(f"Read {len(todo):,} items from {args.file!r}.")
-    
-    for match in sfi(todo, 2, 3).process():
+
+    for match in sfi(todo, max_workers=2, items_per_thread=3, rules_files=args.rules).process():
         print(f"{match[0]}: {', '.join(match[1])}")
